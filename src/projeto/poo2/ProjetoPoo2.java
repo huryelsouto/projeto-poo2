1104857069ed36429f7e61d6cff87515e4272506
huryelsouto <huryel.souto@gmail.com>
2022-05-21 18:51:47 -0300

    finalizando prática 01: implementando padrão strategy

--- src/projeto/poo2/ProjetoPoo2.java
+++ src/projeto/poo2/ProjetoPoo2.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projeto.poo2;
 
-/**
- *
- * @author huryel
- */
+import Context.Personagem;
+import Context.Personagem01;
+import Context.Personagem02;
+import Context.Personagem03;
+
 public class ProjetoPoo2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Personagem p1 = new Personagem01();
+        Personagem p2 = new Personagem02();
+        Personagem p3 = new Personagem03();
+        
+        
+        System.out.println("Comportamentos Personagem 01:");
+        p1.pular();
+        p1.correr();
+        p1.atacar();
+        
+        System.out.println("Comportamentos Personagem 02:");
+        p2.pular();
+        p2.correr();
+        p2.atacar();
+        
+        System.out.println("Comportamentos Personagem 03:");
+        p3.pular();
+        p3.correr();
+        p3.atacar();
+        
     }
     
 }
